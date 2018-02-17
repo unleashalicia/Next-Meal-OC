c9eef05866810f07b6735f81b94d2e3d6c985563
unleashalicia <alicia.adams.evans@gmail.com>
2018-02-17 13:49:11 -0800

    Updated day search for on load

--- php/read.php
+++ php/read.php
@@ -2,19 +2,26 @@
 
     require_once('connection.php');
 
-    $query = "SELECT * FROM next_meal_oc";
+    $query = "SELECT * FROM next_meal_oc WHERE day = {$_GET['search_day']}";
     $result = mysqli_query($conn, $query);
     $output = [
+        'success' => false,
+        'errors' => [],
         'data' => []
     ];
 
 
     if(!empty($result)){
         if(mysqli_num_rows($result) > 0){
+            $output['success'] = true;
             while($row = mysqli_fetch_assoc($result)){
                 $output['data'][] = $row;
             }
+        } else {
+            $output['errors'][] = "No data available.";
         }
+    } else {
+        $output['errors'][] = mysqli_error($conn);
     }
 
 
